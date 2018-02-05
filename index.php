2d103422edda0da41984abad93dccde363545e32
lsanchez <lsanchez@I1110>
2018-02-05 10:52:45 +0100

    segundo commit

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             echo  "primera linea del proyecto";
+            echo "segunda linea del proyecto";
         ?>
     </body>
 </html>
